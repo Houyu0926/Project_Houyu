f7bc7626f06ea8caaa916960a000862fcc40ddca
Houyu <mshehouyu@gmail.com>
2020-05-06 22:27:34 +0200

    Add List

--- app/src/main/java/com/example/projecthouyu/MainActivity.java
+++ app/src/main/java/com/example/projecthouyu/MainActivity.java
@@ -1,15 +1,39 @@
 package com.example.projecthouyu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private ListAdapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        List<String> input = new ArrayList<>();
+        for (int i = 1; i <= 30; i++) {
+            input.add("Team " + i);
+        }
+        // define an adapter
+        mAdapter = new ListAdapter(input);
+        recyclerView.setAdapter(mAdapter);
+
         NBATeams team1 = new NBATeams("OKC","Oklahoma City Thunder", "Billy Donovan", 1967, 1);
 
     }
