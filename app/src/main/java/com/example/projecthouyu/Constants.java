e89549c106c63f8d7f02ba719ce720231eaa6181
Houyu <mshehouyu@gmail.com>
2020-05-08 14:50:33 +0200

    Datas saved

---
+++ app/src/main/java/com/example/projecthouyu/Constants.java
@@ -0,0 +1,6 @@
+package com.example.projecthouyu;
+
+public class Constants {
+    static String KEY_DOG_LIST = "jsonDogList";
+    static String SHARED_PREFERENCES_NAME = "Houyu Application";
+}
--- app/src/main/java/com/example/projecthouyu/MainActivity.java
+++ app/src/main/java/com/example/projecthouyu/MainActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,24 +28,47 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private ListAdapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
+    private SharedPreferences sharedPreferences;
+    private Gson gson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        showList();
-        MakeAPICall();
+        sharedPreferences = getSharedPreferences(Constants.SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE);
+
+        gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        List<Dog> dogList = getDataFromCache();
+
+        if(dogList != null){
+            showList(dogList);
+        }else {
+            MakeAPICall();
+        }
 
         }
+
+    private List<Dog> getDataFromCache() {
+        String jsonDog = sharedPreferences.getString(Constants.KEY_DOG_LIST, null);
+
+        if(jsonDog == null){
+            return null;
+        }else{
+            Type listType = new TypeToken<List<Dog>>(){}.getType();
+            return gson.fromJson(jsonDog, listType);
+        }
+
+
+    }
 
 
     private static final String BASE_URL = "https://raw.githubusercontent.com/Houyu0926/Project_Houyu/master/";
 
     private void MakeAPICall(){
-        Gson gson = new GsonBuilder()
-                .setLenient()
-                .create();
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
@@ -57,6 +84,7 @@ public class MainActivity extends AppCompatActivity {
                 if(response.isSuccessful() && response.body() != null){
                     List<Dog> dogList = response.body().getMessages();
                     Toast.makeText(getApplicationContext(),"API success", Toast.LENGTH_SHORT).show();
+                    saveList(dogList);
                     showList(dogList);
                 }else{
                     error();
@@ -68,6 +96,17 @@ public class MainActivity extends AppCompatActivity {
                 error();
             }
         });
+    }
+
+    private void saveList(List<Dog> dogList) {
+        String jsonString = gson.toJson(dogList);
+
+        sharedPreferences
+                .edit()
+                .putString(Constants.KEY_DOG_LIST, jsonString)
+                .apply();
+        Toast.makeText(getApplicationContext(),"Dog List Saved", Toast.LENGTH_SHORT).show();
+
     }
 
     private void error() {
